1966f497f93f079990fe7787f7b1895cfa737a31
Nexius2 <sylvain.grosset@gmail.com>
2022-05-25 15:49:56 +0200

    Update pum.sh

--- pum.sh
+++ pum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export json from plex
-python3 /mnt/Dev/Projects/plex-user-manager/plex_api_share.py --backup
+python3 plex_api_share.py --backup
 
 # import json to pum
 python3 import_plex_users.py
